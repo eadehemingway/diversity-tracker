ffdf75fb1b6df4a44d995300e9122902305b52f6
eadehemingway <eade.hemingway@gmail.com>
2019-02-16 12:53:23 +0000

    combine draw first donut and update donut functions

--- client/public/src/components/YourOverview/Donut.tsx
+++ client/public/src/components/YourOverview/Donut.tsx
@@ -16,6 +16,8 @@ export class Donut extends React.Component<DonutProps, DonutState>{
             // data:[{'label':null, 'value': 100}, {'label':null, 'value': 100}],
             prevData: [{'label':'Men', 'value':0}],
             data:[{'label':'Men', 'value':0}],
+            // prevData: [],
+            // data:[],
             padAngle: 0, 
             raceColors: ['#6D7596','rgba(211, 101, 67, 1)', '#9DA3B9', '#D36543','#6D7596', '#9DA3B9'],
             genderColors: ['#4D577F','#6D7596', '#9DA3B9'],
@@ -35,31 +37,35 @@ export class Donut extends React.Component<DonutProps, DonutState>{
     componentDidMount(){
 
         this.updateData(this.props)
-        this.drawFirstDonut()
         
     }
 
 
     updateData =(newProps) =>{
         const dataWithNewValues = []
-
         forEach(newProps.data, ( value, key)=> {
             const arc = {label: key, value:value}
             return dataWithNewValues.push(arc)
         })
         
-        const filteredData=  dataWithNewValues.filter(d=>d.value !== 0)
 
+        // const prevData=[]
+        // // console.log('newprops.prevdata', newProps.prevData)
+        // forEach(newProps.prevData, ( value, key)=> {
+        //     const arc = {label: key, value:value}
+        //     return prevData.push(arc)
+        // })
+     
+
+        const filteredData=  dataWithNewValues.filter(d=>d.value !== 0)
         const padAngle = filteredData.length > 1 ? this.props.padAngle: 0
 
+        let prevData = [...this.state.data]
 
 
-        let prevData=this.state.data
 
-        // if(this.state.data.length === 0){
-        //     prevData = [{'label':'Men', 'value':NaN}]
-        // }
-        this.setState({data:dataWithNewValues, prevData, padAngle}, ()=>{
+        const checkedData = dataWithNewValues.length === 0 ? this.state.data : dataWithNewValues
+        this.setState({data:checkedData, prevData, padAngle}, ()=>{
             this.updateDonut()
 
         })
@@ -68,6 +74,11 @@ export class Donut extends React.Component<DonutProps, DonutState>{
     updateDonut = () =>{
 
         const {radius } = this.props
+        const width = radius * 2;
+        const height = radius * 2;
+  
+   
+
         const { raceColors, genderColors, templateColors, padAngle, targetColors} = this.state
         let color;
         if(this.props.template){
@@ -98,110 +109,14 @@ export class Donut extends React.Component<DonutProps, DonutState>{
             return {
                 ...arc,
                 prevArc
-                // {
-                //     data: {label: "label", value: "Men"},
-                //     endAngle: 0.05,
-                //     index: 0,
-                //     padAngle: 0.05,
-                //     startAngle: 0,
-                //     value: NaN
-                // }
             }
         })
-
 
 
         const theArc = arc()
             .outerRadius(radius)
             .innerRadius(radius/1.6);
 
-
-
-        const path = select(`#donut-group-${this.props.donutName}`)
-            .selectAll('path')
-            .data(newDonutWithPrevArc)
-
-        path.enter()
-            .append('g')
-            .attr('class', 'arc')
-            .append('path')
-            .attr('d', theArc)
-            .attr('fill', function(d,i){return color[i]})
-
-        
-        path.exit()
-            .remove()
-
-        path.transition().duration(1000).attrTween("d", createInterpolator)
-        const tooltip = select(`#donut-${this.props.donutName}`)
-            .append('text')
-            .attr('class', 'tooltip-text')
-
-
-    
-
-
-        select(`#donut-${this.props.donutName}`)
-            .selectAll('path')
-            .on('mouseover', (d)=> {
-
-                tooltip.text(`${d.data.label}: ${d.data.value}`)
-                tooltip.style('visibility', 'visible')
-                tooltip.style('fill', '#4D577F')
-                tooltip.attr('dx', `15px`)
-                tooltip.attr('dy', `15px`)
-
-            })
-            .on('mouseout', (d)=> {
-                tooltip.text(d.data.label)
-                .style('visibility', 'hidden')
-
-            })
-        
-
-        function createInterpolator(d) {
-            // here interpolate is taking two objects (the previous arc object and the new arc object),
-            // it can then use these to determine a sort of scale ...
-            const i =interpolate(d.prevArc, d);
-            return function(d) {
-              return theArc(i(d))
-            }
-          
-          }
-
-    }
-
-    drawFirstDonut=()=>{
-
-        const {radius} = this.props
-        const {padAngle} = this.state
-        const width = radius * 2;
-        const height = radius * 2;
-        const { raceColors, genderColors, templateColors, targetColors} = this.state
-        let color;
-   
-
-
-        if(this.props.template){
-            color=templateColors
-        }else if (this.props.target){
-            color= targetColors}else{
-         color = genderColors
-        }
-
-
-        const donut = pie()
-            .padAngle(padAngle)
-            .value(function(d){
-                return d.value
-            })
-            (this.state.data)
-
-
-
-        const theArc = arc()
-        .outerRadius(radius)
-        .innerRadius(radius/1.6);
 
         const svg = select(`#donut-${this.props.donutName}`)
             .attr('width', width)
@@ -211,7 +126,7 @@ export class Donut extends React.Component<DonutProps, DonutState>{
             .attr('transform', 'translate(' + radius + ',' + radius + ')')
 
         const path = svg.selectAll('path')
-            .data(donut)
+            .data(newDonutWithPrevArc)
             .enter()
             .append('g')
             .attr('class', 'arc')
@@ -219,36 +134,63 @@ export class Donut extends React.Component<DonutProps, DonutState>{
             .attr('d', theArc)
             .attr('fill', function(d,i){return color[i]})
         
-            svg.selectAll('path')
-            .data(donut)
-            .enter()
-            .append("text")
-            .attr('class', 'tooltip-text')
-        // const tooltip = svg.selectAll(".arc")
-        //     .append('div')
-        //     .style('position', 'absolute')
-        //     .style('visibility', 'hidden')
-        //     .text('ttttttttttttttext')
+        //     svg.selectAll('path')
+        //     .data(donut)
+        //     .enter()
+        //     .append("text")
+        //     .attr('class', 'tooltip-text')
 
 
-        // select('.arc')
-        //     .on('mouseover', ()=> tooltip.style('visibility', 'visible'))
+        // const path = select(`#donut-group-${this.props.donutName}`)
+        //     .selectAll('path')
+        //     .data(newDonutWithPrevArc)
+
+        // path.enter()
+        //     .append('g')
+        //     .attr('class', 'arc')
+        //     .append('path')
+        //     .attr('d', theArc)
+        //     .attr('fill', function(d,i){return color[i]})
+        
+        path.exit()
+            .remove()
+
+        path.transition().duration(1000).attrTween("d", createInterpolator)
 
 
+        // const tooltip = select(`#donut-${this.props.donutName}`)
+        //     .append('text')
+        //     .attr('class', 'tooltip-text')
+
+        // select(`#donut-${this.props.donutName}`)
+        //     .selectAll('path')
+        //     .on('mouseover', (d)=> {
+        //         tooltip.text(`${d.data.label}: ${d.data.value}`)
+        //         tooltip.style('visibility', 'visible')
+        //         tooltip.style('fill', '#4D577F')
+        //         tooltip.attr('dx', `15px`)
+        //         tooltip.attr('dy', `15px`)
+        //     })
+        //     .on('mouseout', (d)=> {
+        //         tooltip.text(d.data.label)
+        //         .style('visibility', 'hidden')
+        //     })
+        
+
+        function createInterpolator(d) {
+            // here interpolate is taking two objects (the previous arc object and the new arc object),
+            // it can then use these to determine a sort of scale ...
+            const i =interpolate(d.prevArc, d);
+            return function(d) {
+              return theArc(i(d))
+            }
+          }
     }
-            
+     
 
     render(){
-
         return(
-
-
-            
-
                 <svg id={`donut-${this.props.donutName}`} className={`${this.props.className}`}></svg>
-
-
-
         )
     }
 }
